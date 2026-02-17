965d0ec9c1c567bc0fc7d122168135ea934c3c1b
Dunnbiggs <marshall@h2mreferring.com>
2026-02-17 14:42:12 -0800

    Use_transparent_profile_picture_and_remove_background_color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className="about-grid">
                 <div className="about-image">
                     <img
-                        src="/michael-marshall.jpg"
+                        src="/michael-marshall.png"
                         alt="Michael Marshall, PMHNP"
                         className="profile-photo"
                     />
